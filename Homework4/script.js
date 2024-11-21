9b0d54f5909e52f04f1ce3de76c4e73a2d5bbe69
MilenDashinov <milendashinov@gmail.com>
2024-11-21 18:55:43 +0200

    homework part2

---
+++ Homework4/script.js
@@ -0,0 +1,27 @@
+function checkBMIInterpretation() {
+  let weightInKilograms = prompt('Въведи килограми');
+  console.log(weightInKilograms);
+
+  let heightInMeters = prompt('Въведи височина');
+  console.log(heightInMeters);
+
+  let bodyMassIndex =
+    Number(weightInKilograms) / Math.pow(2, Number(heightInMeters));
+  console.log(bodyMassIndex);
+
+  if (bodyMassIndex < 16) {
+    console.log('тежко недохранване');
+  } else if (bodyMassIndex >= 16 && bodyMassIndex < 16.99) {
+    console.log('средно недохранване');
+  } else if (bodyMassIndex >= 17 && bodyMassIndex < 18.49) {
+    console.log('леко недохранване');
+  }
+
+  if (bodyMassIndex < 18.5) {
+    console.log('поднормено тегло ');
+  } else if (bodyMassIndex >= 18.5 && bodyMassIndex < 24.99) {
+    console.log('нормално тегло ');
+  } else if (bodyMassIndex >= 25) {
+    console.log('наднормено тегло ');
+  }
+}
