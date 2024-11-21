c88fd509cba8d5e4c804330adfcfb539afa12f9f
MilenDashinov <milendashinov@gmail.com>
2024-11-21 17:30:35 +0200

    homework 1 and 2

---
+++ Homework2/script.js
@@ -0,0 +1,16 @@
+function greetings() {
+  // взимаме въведената от потребителя стойност за град:
+  let userTown = document.getElementById('town').value;
+
+  // Task 1: Изпишете в конзолата съдържанието на променливата userTown
+  //console.log(userTown);
+
+  // Task 2:
+  if (userTown === 'София') {
+    console.log('Здравей!');
+  } else if (userTown === 'Париж') {
+    console.log('Salut!');
+  } else if (userTown === 'Лондон') {
+    console.log('Hi!');
+  }
+}
---
+++ homework1.js
@@ -0,0 +1,5 @@
+let a = 1;
+let b = 1;
+console.log(a++ + ++a); //3
+console.log(++b + b++); // 3
+console.log(a + b); // 6
